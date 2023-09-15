d2b498605cc7d9acad30ea74ee09d3ff46a52076
Elena <lenarusskih@gmail.com>
2023-09-15 15:30:20 +0300

    Initial commit

---
+++ ColorConverter/Sources/ColorConverter/ColorConverter.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+
+public struct ColorConverter {
+
+    public init() { }
+
+    public func hexToRGB(hex: String) -> (r: Int, g: Int, b: Int)? {
+        guard hex.count == 7, hex.first == "#" else { return nil }
+
+        let rHex = hex.dropFirst(1).prefix(2)
+        let gHex = hex.dropFirst(3).prefix(2)
+        let bHex = hex.dropFirst(5).prefix(2)
+
+        guard let r = Int(String(rHex), radix: 16),
+              let g = Int(String(gHex), radix: 16),
+              let b = Int(String(bHex), radix: 16) else { return nil }
+
+        return (r, g, b)
+    }
+
+    public func rgbToHex(r: Int, g: Int, b: Int) -> String {
+        return String(format: "#%02X%02X%02X", NSNumber(value: r), NSNumber(value: g), NSNumber(value: b))
+    }
+}
+
---
+++ ColorConverter/Tests/ColorConverterTests/ColorConverterTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ColorConverter
+
+final class ColorConverterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ColorConverter().text, "Hello, World!")
+    }
+}
